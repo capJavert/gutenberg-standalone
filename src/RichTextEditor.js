c0fca92c35d730a10c46689286544dae9d0e6183
capJavert <ante.baric3@gmail.com>
2020-02-23 12:13:16 +0100

    demo

---
+++ src/RichTextEditor.js
@@ -0,0 +1,64 @@
+/**
+ * WordPress dependencies
+ */
+import React, { useEffect, useState } from 'react'
+import {
+	BlockEditorKeyboardShortcuts,
+	BlockEditorProvider,
+	BlockList,
+	BlockInspector,
+	WritingFlow,
+	ObserveTyping,
+} from '@wordpress/block-editor';
+import {
+	Popover,
+	SlotFillProvider,
+	DropZoneProvider,
+} from '@wordpress/components';
+import { registerCoreBlocks } from '@wordpress/block-library';
+import '@wordpress/format-library';
+
+/**
+ * Internal dependencies
+ */
+
+const RichTextEditor = () => {
+	const [ blocks, updateBlocks ] = useState( [] );
+
+	useEffect( () => {
+		console.log('DEBUG: gutenberg-state', blocks)
+	}, [blocks] );
+
+	useEffect( () => {
+		registerCoreBlocks();
+	}, [] );
+
+	return (
+		<div className="playground">
+			<SlotFillProvider>
+				<DropZoneProvider>
+					<BlockEditorProvider
+						value={ blocks }
+						onInput={ updateBlocks }
+						onChange={ updateBlocks }
+					>
+						<div className="playground__sidebar">
+							<BlockInspector />
+						</div>
+						<div className="editor-styles-wrapper">
+							<BlockEditorKeyboardShortcuts />
+							<WritingFlow>
+								<ObserveTyping>
+									<BlockList />
+								</ObserveTyping>
+							</WritingFlow>
+						</div>
+						<Popover.Slot />
+					</BlockEditorProvider>
+				</DropZoneProvider>
+			</SlotFillProvider>
+		</div>
+	);
+}
+
+export default RichTextEditor
